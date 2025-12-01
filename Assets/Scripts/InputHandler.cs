a618b57d392ce42072f3acf7abf6cba8752f5991
Tanisha-kothari <kotharitanishanilesh@gmail.com>
2025-12-01 18:15:47 +0530

    Added Trail Renderer and Touch detection

---
+++ Assets/Scripts/InputHandler.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InputHandler : MonoBehaviour
+{
+    private Input input;
+    float depth = 10f;
+    public GameObject trail;
+
+    private void Awake()
+    {
+        input = new Input();
+    }
+    private void OnEnable()
+    {
+        input.Enable();
+
+        input.Touch.TouchPress.performed += OnTouchPress;
+        input.Touch.TouchPosition.performed += TouchPosition;
+    }
+
+    private void OnDisable()
+    {
+        input.Disable();
+    }
+
+    public void TouchPosition(InputAction.CallbackContext context)
+    {
+
+        Debug.Log("Touch Pos called");
+        Vector2 fingerPos = context.ReadValue<Vector2>();
+
+        Ray ray = Camera.main.ScreenPointToRay(fingerPos);
+
+        if(Physics.Raycast(ray, out RaycastHit hit))
+        {
+            Vector3 point = hit.point;
+            point.z = -3;
+
+            trail.transform.position = point;
+        }
+
+    }
+
+    public void OnTouchPress(InputAction.CallbackContext context)
+    {
+
+    }
+
+}
