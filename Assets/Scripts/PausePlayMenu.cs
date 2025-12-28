bc81ddd41bd1494b04430a78a0f75c2b5dd9b555
Tanisha-kothari <kotharitanishanilesh@gmail.com>
2025-12-28 18:24:34 +0530

    added homepage and designing

--- Assets/Scripts/PausePlayMenu.cs
+++ Assets/Scripts/PausePlayMenu.cs
@@ -1,9 +1,12 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PausePlayMenu : MonoBehaviour
 {
     [SerializeField] GameObject menu;
     [SerializeField] GameTimer gameTimer;
+
+    [SerializeField] string sceneName = "HomePage";
 
     private void Awake()
     {
@@ -22,5 +25,13 @@ public class PausePlayMenu : MonoBehaviour
         menu.SetActive(false);
         Time.timeScale = 1f;
         gameTimer.StartTimer();
+    }
+
+    public void GoHome()
+    {
+        menu.SetActive(false);
+        Time.timeScale = 0f;
+        gameTimer.StopTimer();
+        SceneManager.LoadScene(sceneName);
     }
 }
